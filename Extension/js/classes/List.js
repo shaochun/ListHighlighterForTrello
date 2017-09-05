47449948b09483f2db3605ea0c25fc270583764e
Mark Kenny <beingmrkenny@gmail.com>
2017-09-05 20:14:23 +0100

    Whitespace and changing notice markup and code

--- Extension/js/classes/List.js
+++ Extension/js/classes/List.js
@@ -5,35 +5,22 @@ class List {
 	// on card text change - countCards
 	// on add/remove card - countCards
 
-    constructor (list) {
+	constructor (list) {
 		// NOTE list is .list
-        this.list = list;
-    }
+		this.list = list;
+	}
 
-    updateCardLimit () {
+	updateCardLimit () {
 
-		var limit = this.getLimitFromTitle(),
-			roundel = this.list.querySelector('.bmko_list-limit-roundel');
+		var limit = this.getLimitFromTitle();
 
 		if (limit) {
-
-	        this.list.dataset.bmkoListLimit = limit;
-
-			if (!roundel) {
-				roundel = document.createElement('span');
-				roundel.classList.add('bmko_list-limit-roundel');
-				let listHeader = this.list.querySelector('.list-header'),
-					numCards = listHeader.querySelector('.list-header-num-cards');
-				listHeader.insertBefore(roundel, numCards);
-			}
-
-			roundel.textContent = `Limit: ${limit}`;
-
-		} else if (roundel) {
-			roundel.remove();
+			this.list.dataset.bmkoListLimit = limit;
+		} else {
+			this.list.dataset.bmkoListLimit = 'none';
 		}
 
-    }
+	}
 
 	getLimitFromTitle () {
 		var title = this.list.querySelector('.list-header-name-assist').textContent,
@@ -44,25 +31,20 @@ class List {
 		}
 	}
 
-    countCards () {
+	countCards () {
 
 		var listLimit = this.list.dataset.bmkoListLimit;
 
-		if (listLimit) {
+		if (listLimit && listLimit !== 'none') {
 
 			var cards = this.list.querySelectorAll('.list-card:not(.bmko_header-card-applied)'),
-	            cardCount = cards.length;
-
-			console.log(cards);
-			console.log(cardCount);
+				cardCount = cards.length;
 
 			listLimit = parseInt(listLimit);
 
-			console.log(cardCount, listLimit);
-
-	        for (let i = cards.length-1; i>-1; i--) {
-	            cardCount += cards[i].dataset.bmkoPoints || 0;
-	        }
+			for (let i = cards.length-1; i>-1; i--) {
+				cardCount += cards[i].dataset.bmkoPoints || 0;
+			}
 
 			this.updateStatusNotice(cardCount, listLimit);
 
@@ -71,39 +53,46 @@ class List {
 			}
 		}
 
-    }
-
-    updateStatusNotice (cardCount, listLimit) {
-		if (cardCount > listLimit) {
-            this.updateNotice('This has too many on it'); // TODO better message
-        } else if (cardCount < listLimit) {
-			let notice = this.list.querySelector('.notice');
-            if (notice) {
-            	notice.remove();
-            }
-        } else {
-			this.updateNotice('This is full'); // TODO better message
-        }
-    }
-
-	updateNotice (message) {
-		var notice = this.list.querySelector('.notice');
-		if (!notice) {
-			let listCards = this.list.querySelector('.list-cards'),
-				firstCard = listCards.querySelector('.list-card');
-			notice = document.createElement('div');
-			notice.classList.add('notice');
-			listCards.insertBefore(notice, firstCard);
-		}
-		notice.textContent = message;
 	}
 
-    updateRefuseCardStatus (cardCount, listLimit) {
+	updateStatusNotice (cardCount, listLimit) {
+
+		var notice = this.list.querySelector('.bmko_list-limit-notice');
+
+		if (!notice) {
+			let p1 = document.createElement('p'),
+				p2 = document.createElement('p');
+			notice = document.createElement('div');
+			notice.appendChild(p1);
+			notice.appendChild(p2);
+			notice.classList.add('bmko_list-limit-notice');
+			let listHeader = this.list.querySelector('.list-header'),
+				numCards = listHeader.querySelector('.list-header-num-cards');
+			listHeader.insertBefore(notice, numCards);
+		}
+
+		notice.firstElementChild.textContent = `Limit: ${listLimit}`;
+
+		if (cardCount > listLimit) {
+			// TODO better message
+			notice.firstElementChild.nextElementSibling.innerHTML = 'This has too many on it';
+		} else if (cardCount < listLimit) {
+			let notice = this.list.querySelector('.notice');
+			if (notice) {
+				notice.remove();
+			}
+		} else {
+			// TODO better message
+			notice.firstElementChild.nextElementSibling.innerHTML = 'This is full';
+		}
+	}
+
+	updateRefuseCardStatus (cardCount, listLimit) {
 		if (cardCount >= listLimit) {
 			// TODO refuse them
 		} else {
 			// TODO let them in
 		}
-    }
+	}
 
 }
